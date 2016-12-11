353e4b5d937906ce9e8faf9d006b11310e3a2860
Kezzo <jon_jelinski@t-online.de>
2016-12-11 02:25:33 +0100

    Initial project commit

---
+++ Assets/Scripts/Root.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Root : MonoBehaviour
+{
+    [SerializeField]
+    private string m_initialSceneToLoad;
+
+    [SerializeField]
+    private CoroutineHelper m_coroutineHelper;
+    public CoroutineHelper CoroutineHelper { get { return m_coroutineHelper; } }
+
+    // Singleton
+    private static Root m_instance = null;
+    public static Root Instance
+    {
+        get { return m_instance; }
+    }
+
+    private void Awake()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this;
+        }
+        else if (m_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        Initialize();
+    }
+
+    /// <summary>
+    /// Initializes the root.
+    /// </summary>
+    private void Initialize()
+    {
+        SceneManager.LoadScene(m_initialSceneToLoad, LoadSceneMode.Additive);
+    }
+}
