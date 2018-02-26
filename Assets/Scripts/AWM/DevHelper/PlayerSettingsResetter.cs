9bf88c977267f398ad0ee85031d9ad8a09937230
Kezzo <jon_jelinski@t-online.de>
2018-02-26 18:49:54 +0200

    [#148] Implement unlock all levels cheat

--- Assets/Scripts/AWM/DevHelper/PlayerSettingsResetter.cs
+++ Assets/Scripts/AWM/DevHelper/PlayerSettingsResetter.cs
@@ -1,4 +1,6 @@
 ﻿#if UNITY_EDITOR
+using AWM.Models;
+using AWM.System;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,8 +12,35 @@ namespace AWM.DevHelper
         private static void ResetPlayerPrefs()
         {
             PlayerPrefs.DeleteAll();
+            Debug.Log("Resetted PlayerPrefs!");
         }
-	
+
+        [MenuItem("Tools/UnlockAllLevels")]
+        private static void UnlockAllLevels()
+        {
+            int levelCount = 1;
+
+            while (true)
+            {
+                string levelName = string.Format("Levels/Level{0}", levelCount);
+                MapGenerationData mapGenerationData = CC.ADS.GetAssetDataAtPath<MapGenerationData>(levelName);
+
+                if (mapGenerationData == null)
+                {
+                    break;
+                }
+                else
+                {
+                    CC.PPS.TrackLevelAsCompleted(mapGenerationData.m_LevelName);
+                    CC.PPS.LastUnlockedLevel = mapGenerationData.m_LevelName;
+                }
+
+                levelCount++;
+            }
+
+            Debug.Log("Unlocked all Levels!");
+        }
+
     }
 }
 #endif
